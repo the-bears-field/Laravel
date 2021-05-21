351f62fbe09444f287be495f5be268ce05b2e6d6
satoshi_kumano <thebearsfield@gmail.com>
2021-05-21 14:57:59 +0900

    「Laravel入門第2版」p.32〜35を参考に、routes/web.phpを編集。
    /helloへのルーティングを追加。
    htmlの内容をヒアドキュメント構文で記述し、$htmlに代入。

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -12,7 +12,35 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+$html = <<< HTML
+    <html lang="ja">
+    <head>
+    <title>Hello</title>
+    <style>
+    body {
+        color: #999;
+        font-size: 16pt;
+    }
+    h1 {
+        color: #eee;
+        font-size: 100pt;
+        margin: -40px 0px -50px 0px;
+        text-align: right;
+    }
+    </style>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <p>This is sample page.</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    HTML;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('hello', function () use ($html) {
+    return $html;
 });
