d6d91e3461e91c43b0e8213cdf071618b859c0c5
satoshi_kumano <thebearsfield@gmail.com>
2021-05-29 18:41:05 +0900

    「Laravel入門第2版」p.46〜48を参考に、コントローラおよびルート情報修正。
    HelloController.phpを修正。
    クラス外にHTMLの構造を格納する変数を宣言、各項目を定義。
    tagを生成する関数を書籍を参考に、引数の型指定の設定、開始タグ内の属性の有無で処理を変えるなど手を加えた上で追加。
    HelloControllerクラスのindexアクションメソッドの引数を削除。
    メソッド内で$htmlにHTMLの構造を定義し、返り値に設定。
    otherアクションメソッド追加。
    メソッド内で$htmlにHTMLの構造を定義し、返り値に設定。
    
    routes/web.phpの"/hello"へのルート情報を下記の通りに変更。
    Route::getの第1引数にルートパラメータを'hello'に戻した。
    routes/web.phpの"/hello/other"へのルート情報を追記、
    第2引数はHelloControllerのotherアクションを設定。

--- myapp/app/Http/Controllers/HelloController.php
+++ myapp/app/Http/Controllers/HelloController.php
@@ -4,35 +4,52 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+
+$head  = '<html><head>';
+$style = <<< EOF
+    <style>
+    body {
+        color: #999;
+        font-size: 16pt;
+    }
+    h1 {
+        color: #eee;
+        font-size: 100pt;
+        margin: -40px 0px -50px 0px;
+        text-align: center;
+    }
+    </style>
+EOF;
+$body  = '</head><body>';
+$end   = '</body></html>';
+
+function tag(string $tag, string $txt, string $attributes = '') {
+    $endTag = $tag;
+
+    if($attributes) {
+        $tag = $tag. ' '. $attributes;
+    }
+
+    return "<{$tag}>". $txt. "</{$endTag}>";
+}
+
 class HelloController extends Controller
 {
-    public function index($id = 'noname', $pass = 'unknown') {
-        return <<< HTML
-        <html>
-        <head>
-        <title>Hello/index</title>
-        <style>
-        body {
-            color: #999;
-            font-size: 16pt;
-        }
-        h1 {
-            color: #eee;
-            font-size: 100pt;
-            margin: -40px 0px -50px 0px;
-            text-align: center;
-        }
-        </style>
-        </head>
-        <body>
-        <h1>Index</h1>
-        <p>これは、Helloコントローラのindexアクションです。</p>
-        <ul>
-            <li>ID: {$id}</li>
-            <li>PASS: {$pass}</li>
-        </ul>
-        </body>
-        </html>
-        HTML;
+    public function index() {
+        global $head, $style, $body, $end;
+
+        $html = $head. tag('title', 'Hello/Index'). $style. $body
+        . tag('h1', 'Index'). tag('p', 'this is Index page')
+        . tag('a', 'go to Other page', 'href="/hello/other"')
+        . $end;
+        return $html;
+    }
+
+    public function other() {
+        global $head, $style, $body, $end;
+        $html = $head. tag('title', 'Hello/Other'). $style. $body
+        . tag('h1', 'Other'). tag('p', 'this is Other page'). $end;
+        return $html;
     }
 }
