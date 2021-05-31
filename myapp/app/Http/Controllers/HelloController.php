6c3ccf8e357d231260653f089f75a4eec79c8211
satoshi_kumano <thebearsfield@gmail.com>
2021-05-31 14:39:37 +0900

    「Laravel入門第2版」p.62を参考に、コントローラ、ルーティング情報を修正。
    HelloControllerクラスのindexアクションメソッドの返り値を、'hello.index'のビューを返すよう修正。
    
    routes/web.phpの"/hello"へのルート情報を修正。
    第2引数をクロージャからHelloControllerクラスのindexアクションメソッドを参照するように変更。

--- myapp/app/Http/Controllers/HelloController.php
+++ myapp/app/Http/Controllers/HelloController.php
@@ -7,35 +7,7 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response) {
-        $html = <<< EOF
-        <html>
-        <head>
-        <title>Hello/Index</title>
-        <style>
-        body {
-            color: #999;
-            font-size: 16pt;
-        }
-        h1 {
-            color: #fafafa;
-            font-size: 120pt;
-            margin: -50px 0px -120px 0px;
-            text-align: right;
-        }
-        </style>
-        </head>
-        <body>
-        <h1>Hello</h1>
-        <h3>Request</h3>
-        <pre>{$request}</pre>
-        <h3>Response</h3>
-        <pre>{$response}</pre>
-        </body>
-        </html>
-        EOF;
-
-        $response->setContent($html);
-        return $response;
+    public function index() {
+        return view('hello.index');
     }
 }
