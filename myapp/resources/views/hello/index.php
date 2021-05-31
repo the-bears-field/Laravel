b15851112651b1b34e0af540bb8163b6102ae218
satoshi_kumano <thebearsfield@gmail.com>
2021-05-31 14:31:09 +0900

    「Laravel入門第2版」p.58〜62を参考に、ビューファイル生成、ルーティング情報を修正。
    /resources/viewディレクトリ下でhelloディレクトリ作成。
    helloディレクトリ下でindex.php生成。
    index.phpにHTMLを記述。
    
    routes/web.phpの"/hello"へのルート情報を修正。
    第2引数をクロージャに変更、返り値として上記生成したビューを返すように修正。

---
+++ myapp/resources/views/hello/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Hello/Index</title>
+<style>
+body {
+    color: #999;
+    font-size: 16pt;
+}
+h1 {
+    color: #f6f6f6;
+    font-size: 100pt;
+    margin: -50px 0px -100px 0px;
+    text-align: right;
+}
+</style>
+</head>
+<body>
+<h1>Index</h1>
+<p>This is sample page with php-template.</p>
+</body>
+</html>
