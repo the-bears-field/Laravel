eb008f3b1edc926c3c772a8c07ab8fe5522f0e0c
satoshi_kumano <thebearsfield@gmail.com>
2021-06-01 14:55:42 +0900

    「Laravel入門第2版」p.69〜73を参考に、ビュー、コントローラ、ルーティング修正。
    /resorces/view/helloディレクトリ下のindex.blade.phpを修正。
    フォームを追加。
    フォームの中身にCSRF対策目的のBladeディレクティブを記載、テキストフォーム、送信ボタン追加。
    
    HelloControllerクラスにpostアクションメソッド追加。
    引数はRequestインスタンスを設定。
    ローカル変数(連想配列)に、引数のインスタンスにあるmsgプロパティの値を代入。
    返り値としてビューメソッドを追加。
    第1引数はhelloディレクトリ下のindex.blade.php、第2引数は上記ローカル変数を設定した。
    
    ルーティングファイルにPOST通信での処理としてRoute::postメソッドを追加。
    Route::postメソッドの第1引数は'hello'、第2引数は'HelloController@post'を設定した。

--- myapp/app/Http/Controllers/HelloController.php
+++ myapp/app/Http/Controllers/HelloController.php
@@ -13,4 +13,12 @@ class HelloController extends Controller
         ];
         return view('hello.index', $data);
     }
+
+    public function post(Request $request) {
+        $msg  = $request->msg;
+        $data = [
+            'msg' => 'こんにちは、'. $msg. 'さん！'
+        ];
+        return view('hello.index', $data);
+    }
 }
