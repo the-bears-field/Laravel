9eb30708ea1de76ee61801de236cb749ea261d38
the-bears-field <thebearsfield@gmail.com>
2021-06-07 07:40:35 +0900

    「Laravel入門第2版」p.76〜78を参考に、ビュー、コントローラを修正。 /resorces/view/helloディレクトリ下のindex.blade.phpを修正。 条件分岐の処理を@ifから@issetに変更。
    HelloControllerクラスのindexアクションメソッド内のviewメソッドの第2引数の値を削除。

--- myapp/app/Http/Controllers/HelloController.php
+++ myapp/app/Http/Controllers/HelloController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        return view('hello.index', ['msg' => '']);
+        return view('hello.index');
     }
 
     public function post(Request $request) {
