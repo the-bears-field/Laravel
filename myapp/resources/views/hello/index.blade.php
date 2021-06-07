3957c236199bd0fd3ecf609043fdad72e5fe772e
the-bears-field <thebearsfield@gmail.com>
2021-06-08 03:10:21 +0900

    「Laravel入門第2版」p.85〜90を参考に、ビューを追加、修正。 /resorces/view/layoutsディレクトリ作成。 /resorces/view/layoutsディレクトリ下にベースレイアウトとしてhelloapp.blade.php作成。 @yield、@sectionディレクティブを使い記述。 親テンプレートであるため@sectionディレクティブの閉じタグは@showを使用。
    /resorces/view/helloディレクトリ下のindex.blade.phpを修正。
    1行目に@extendsディレクティブを使いベースレイアウトを継承する処理を追加。
    
    タイトルにあたる箇所を@sectionディレクティブを使い記述。
    出力内容は@sectionディレクティブの第2引数に記述。
    
    メニューバーにあたる箇所を@sectionディレクティブを使い記述。
    @parentディレクティブを使い、ベースレイアウトから継承した上で出力するようにした。
    
    コンテンツにあたる箇所を@sectionディレクティブを使い記述。
    @sectionディレクティブ内にHTMLを記述。
    
    フッターにあたる箇所を@sectionディレクティブを使い記述。
    @sectionディレクティブ内に出力内容を記述。

--- myapp/resources/views/hello/index.blade.php
+++ myapp/resources/views/hello/index.blade.php
@@ -1,34 +1,16 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-    body {
-        font-size:16pt;
-        color:#999;
-    }
+@extends('layouts.helloapp')
 
-    h1 {
-        font-size:50pt;
-        text-align:right;
-        color:#f6f6f6;
-        margin:-20px 0px -30px 0px;
-        letter-spacing:-4pt;
-    }
-    </style>
-    </head>
-    <body>
-    <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブの例</p>
-    <ol>
-    @php
-        $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-        <li>{{ $data[$counter] }}</li>
-        @php
-            $counter++;
-        @endphp
-    @endwhile
-    </ol>
-    </body>
-</html>
+@section('title', 'index')
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+    copyright 2020 tuyano.
+@endsection
