13efa785985a71bd429de6db1fb6c590bff552cf
the-bears-field <thebearsfield@gmail.com>
2021-06-12 14:52:57 +0900

    「Laravel入門第2版」p.110〜113を参考にミドルウェアの処理を反映させるため、諸々修正。 前回生成した、/app/Http/Middleware/HelloMiddleware.phpを修正。 多次元連想配列を格納する変数を定義。 第1引数のRequestインスタンスのmergeメソッドを使用し、既存のRequestインスタンスに反映させた。 引数に連想配列を定義。 キーはRequestインスタンスのプロパティとして追加する任意の文字列('data')とし、 バリューは先程定義した多重連想配列を格納した変数($data)を指定。
    /routes/web.phpを編集し、ルーティング情報を修正。
    ミドルウェアの利用のため名前空間(App\Http\Middleware\HelloMiddleware)を使用する処理を追加。
    "/hello"へのアクセス時に処理されるRoute::getメソッドの後にメソッドチェーン(middlewareメソッド)追加。
    引数はHelloMiddlewareクラスのクラス名が入るように処理。
    
    ミドルウェア利用のため、/app/Http/Controllers/HelloController.phpを修正。
    HelloControllerクラスのindexメソッドの引数にRequestインスタンスを指定、
    返り値に指定しているviewメソッドの第2引数の連想配列を変更。
    キーはビューファイルに引き渡す任意の文字列('data')とし、
    バリューはミドルウェアで追加したRequestインスタンスのプロパティ($request->data)を指定。
    
    ミドルウェア利用のため、resources/views/hello/index.blade.phpを修正。
    不要な箇所を削除。
    tableタグと@foreachディレクティブを追加。
    @foreachディレクティブの引数にコントローラファイルから引き渡された配列($data)を指定。
    @foreachディレクティブ中にtr、th、tdタグ、ミドルウェアで定義した配列データ出力する処理を追加。

--- myapp/app/Http/Middleware/HelloMiddleware.php
+++ myapp/app/Http/Middleware/HelloMiddleware.php
@@ -16,6 +16,12 @@ class HelloMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        $data = [
+            ['name' => 'taro', 'mail' => 'taro@yamada'],
+            ['name' => 'hanako', 'mail' => 'hanako@flower'],
+            ['name' => 'sachiko', 'mail' => 'sachiko@happy']
+        ];
+        $request->merge(['data' => $data]);
         return $next($request);
     }
 }
