3261b3292e90cceb13dfb6bd6d3f363a94a51645
the-bears-field <thebearsfield@gmail.com>
2021-06-14 13:58:48 +0900

    「Laravel入門第2版」p.119〜120を参考に、ミドルウェアのグループ登録するため諸々修正。 /app/Http/kernel.php編集。 前回、Kernelクラスのmiddlewareプロパティに登録した箇所を削除。 KernelクラスのmiddlewareGroupsプロパティに新しく要素を追加。 キーは'helo'としバリューは配列を挿入。 挿入した配列に登録したいミドルウェアのクラス名(\App\Http\Middleware\HelloMiddleware::class)を記載。
    /routes/web.php修正。
    "/hello"へのアクセス時に処理されるRoute::getメソッドの後に再びメソッドチェーン(middlewareメソッド)追加。
    引数はKernelクラスのmiddlewareGroupsプロパティに新しく追加した要素のキー('helo')を記述。

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', 'HelloController@index')->middleware('helo');
 Route::post('hello', 'HelloController@post');
