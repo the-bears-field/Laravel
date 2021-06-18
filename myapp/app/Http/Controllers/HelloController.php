1f97c820c924331e21d66b7783ede70116fc84e4
the-bears-field <thebearsfield@gmail.com>
2021-06-18 14:56:41 +0900

    「Laravel入門第2版」p.144〜146やリファレンスを参考にコントローラを編集。 Validatorをインポートし、バリデータを使用できるように修正。 ※下記の記述でもインポート可能。 use Illuminate\Support\Facades\Validator;
    postアクションメソッドの引数の型をRequestクラスに戻した。
    postアクションメソッド中で、Validator::makeメソッドの第2引数で用いる検証ルール($validate_rule)を連想配列で定義。
    Validator::makeメソッドを実行。
    第1引数はRequestインスタンスのallメソッド(返り値は連想配列)、第2引数は先程定義した検証ルールを格納した変数を記述。
    返り値として渡されたValidatorインスタンスを変数($validator)に格納。
    if文を追加、Validatorインスタンスのfailsメソッドを実行し、検証結果の失敗の有無を判定。
    検証の失敗が存在していれば、'/hello'へ推移するよう処理。
    下記の通りチェーンメソッドを使った処理を実装。
    redirectヘルパーを使いRedirectResponseインスタンス生成。
    その後、RedirectResponseインスタンスのwithErrorsメソッドを実行し、エラーメッセージをセッションに一時保持保存。
    引数はValidatorインスタンス($validator)を使用。
    ページ推移後にエラーメッセージを変数($errors)に格納。
    その後、RedirectResponseインスタンスのwithInputメソッドを実行し、ページ推移前に入力していたデータをセッションで保持。
    
    参考URL
    https://laravel.com/api/8.x/Illuminate/Contracts/Validation/Validator.html#method_fails
    https://laravel.com/api/8.x/Illuminate/Http/RedirectResponse.html#method_withErrors
    https://laravel.com/docs/8.x/redirects

--- myapp/app/Http/Controllers/HelloController.php
+++ myapp/app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -12,7 +13,18 @@ class HelloController extends Controller
         return view('hello.index', ['msg' => 'フォームを入力：']);
     }
 
-    public function post(HelloRequest $request) {
+    public function post(Request $request) {
+        $validate_rule = [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ];
+        $validator = Validator::make($request->all(), $validate_rule);
+
+        if ($validator->fails()) {
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
+
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
