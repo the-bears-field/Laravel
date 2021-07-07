0b9ee231629dc01bd68c882367cc416daeb3effa
the-bears-field <thebearsfield@gmail.com>
2021-07-07 14:35:51 +0900

    「Laravel入門第2版」p.205〜207を参考に、データベース接続時の処理方法を変更。 DB::tableメソッドを使いIlluminate\Database\Query\Builderインスタンスを取得。 引数は取得したいテーブル名('people')とした。 さらにBuilderインスタンスのgetメソッドを実行。 Illuminate\Support\Collectionインスタンスを取得し、変数($items)に格納。
    参考URL
    https://laravel.com/api/8.x/Illuminate/Support/Facades/DB.html#method_table
    https://laravel.com/api/8.x/Illuminate/Database/Connection.html#method_table
    https://laravel.com/api/8.x/Illuminate/Database/Query/Builder.html#method_get
    https://laravel.com/api/8.x/Illuminate/Support/Collection.html

--- myapp/app/Http/Controllers/HelloController.php
+++ myapp/app/Http/Controllers/HelloController.php
@@ -11,12 +11,7 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        if (isset($request->id)) {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from people where id = :id', $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
+        $items = DB::table('people')->get();
         return view('hello.index', ['items' => $items]);
     }
 
