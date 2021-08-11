46102aa8826834f86c25aedc0eef29965e234907
the-bears-field <thebearsfield@gmail.com>
2021-08-11 21:08:35 +0900

    「Laravel入門第2版」p.282〜283を参考に、モデル、スコープ編集。
    ScopePersonクラスのapplyメソッドの処理を、
    BoardモデルのbelongsTo接続に影響を与えるため削除。
    
    Boardモデル編集。
    Illuminate\Database\Eloquent\Relations\BelongsToをインポート。
    personメソッド追加。
    返り値はBelongsToインスタンスを指定。
    personメソッド中の処理で、自身のbelongsToメソッドの実行結果を返り値を返すよう処理。
    getDataメソッドの処理内容を変更。
    自身のpersonプロパティに格納されているPersonインスタンスからnameプロパティを出力する処理を追加。
    
    参考URL
    https://laravel.com/api/8.x/Illuminate/Database/Eloquent/Model.html#method_belongsTo
    https://laravel.com/api/8.x/Illuminate/Database/Eloquent/Relations/BelongsTo.html

--- myapp/app/Models/Board.php
+++ myapp/app/Models/Board.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Board extends Model
 {
@@ -16,8 +17,13 @@ class Board extends Model
         'message' => 'required'
     ];
 
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(Person::class);
+    }
+
     public function getData()
     {
-        return $this->id. ': '. $this->title;
+        return $this->id. ': '. $this->title. ' ('. $this->person->name. ')';
     }
 }
