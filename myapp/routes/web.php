a0e24e38d20bdb3ff2bf7de0b82d4a35622f93bc
the-bears-field <thebearsfield@gmail.com>
2021-08-14 16:40:02 +0900

    「Laravel入門第2版」p.290〜299を参考に、諸々ファイル生成、実行。 マイグレーションファイル生成、カラム構造を定義した上でマイグレーション実行し、データベースにrestdataテーブルを追加。
    Restdataモデル、RestdataTableシーダ生成。
    DatabaseSeeder.phpを編集し、先程生成したシーダファイルが反映するように修正。
    
    Restappコントローラ生成。
    コマンドでの生成時に、"--resource"オプションを付与することで、あらかじめアクションを用意した。
    
    /routes/web.php編集し、新たなルーティングを追加。
    Routeクラスのresourceメソッドを使い、"/rest"下のルーティングを一括設定。
    
    参考URL
    https://readouble.com/laravel/8.x/ja/controllers.html#resource-controllers
    https://laravel.com/api/8.x/Illuminate/Routing/Router.html#method_resource

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -41,3 +41,5 @@ Route::post('person/del', 'PersonController@remove');
 Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+Route::resource('rest', 'RestappController');
