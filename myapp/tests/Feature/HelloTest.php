037c130fd84b5ccbf9399f2fb0ff226e8df88c4f
the-bears-field <thebearsfield@gmail.com>
2021-08-31 13:57:51 +0900

    「Laravel入門第2版」p.340〜343を参考に、テスト作成。 appコンテナに入り、myappディレクトリ下で下記コマンドを実行。 php artisan make:test HelloTest
    /tests/featureディレクトリ下に生成されたテストファイル(HelloTest.php)を編集。
    testHelloメソッドを追加し、メソッド内でテストを実行する処理を記述。
    
    再びappコンテナに入り、myappディレクトリ下で下記コマンドいずれかを実行しテストを実行。
    vendor/bin/phpunit
    php artisan test
    
    https://readouble.com/laravel/8.x/ja/testing.html
    https://laravel.com/api/8.x/Illuminate/Foundation/Testing/TestCase.html
    https://phpunit.readthedocs.io/ja/latest/assertions.html

---
+++ myapp/tests/Feature/HelloTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testHello()
+    {
+        $this->assertTrue(true);
+
+        $arr = [];
+        $this->assertEmpty($arr);
+
+        $msg = 'Hello';
+        $this->assertEquals('Hello', $msg);
+
+        $n = random_int(0, 100);
+        $this->assertLessThan(100, $n);
+    }
+}
