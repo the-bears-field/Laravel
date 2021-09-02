9552521cbd638cc4b510dee593a66e7e3c8f4e9c
the-bears-field <thebearsfield@gmail.com>
2021-09-02 14:28:55 +0900

    「Laravel入門第2版」p.343〜346を参考に、Webページアクセスのテストを実装。 /tests/Feature/HelloTest.php編集 Illuminate\Foundation\Testing\DatabaseMigrationsインポート。 App\Models\Userインポート。 HelloTestクラス内でDatabaseMigrationsを継承。 HelloTestクラスのgetメソッドを実行し、アプリケーションにGETリクエストを送信。 返り値としてIlluminate\Testing\TestResponseインスタンスを取得し、変数($response)に格納。 TestResponseインスタンスのassertStatusメソッドを実行し、HTTPステータスコードが引数の値と合致するか検証。 User::factoryメソッドを実行しFactoryインスタンスを取得。 次いでcreateメソッドを実行し、データベースにレコードを挿入した上でUserインスタンス取得。 HelloTestクラスのactingAsメソッドを実行し、引数のUserインスタンスをログイン中のユーザとして設定。 次いでgetメソッドを実行し、TestResponseインスタンスを取得し変数に格納。 最後にTestResponseインスタンスのassertStatusメソッドを実行し、HTTPステータスコードを検証。
    参考URL
    https://www.oulub.com/docs/laravel/ja-jp/database-testing#using-factories
    https://laravel.com/api/8.x/Illuminate/Database/Eloquent/Factories/HasFactory.html
    https://laravel.com/api/8.x/Illuminate/Database/Eloquent/Factories/Factory.html
    https://qiita.com/ucan-lab/items/788d1a603f2b53ebb999
    https://laravel.com/api/8.x/Illuminate/Foundation/Testing/Concerns/InteractsWithAuthentication.html#method_actingAs

--- myapp/tests/Feature/HelloTest.php
+++ myapp/tests/Feature/HelloTest.php
@@ -4,10 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
+use App\Models\User;
 
 class HelloTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
@@ -17,13 +20,17 @@ class HelloTest extends TestCase
     {
         $this->assertTrue(true);
 
-        $arr = [];
-        $this->assertEmpty($arr);
+        $response = $this->get('/');
+        $response->assertStatus(200);
 
-        $msg = 'Hello';
-        $this->assertEquals('Hello', $msg);
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
 
-        $n = random_int(0, 100);
-        $this->assertLessThan(100, $n);
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
     }
 }
